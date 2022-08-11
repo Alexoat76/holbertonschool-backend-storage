780cf1628d6cec64d1a23b7ea9bbd78ace8ccc18
Alexoat76 <3915@holbertonschool.com>
2022-08-11 11:27:31 -0500

    SQL script | Lists all bands with Glam rock as their main style | Ranked by their longevity :heavy_check_mark:

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,5 +3,5 @@
 -- Column names must be: band_name and lifespan (in years)
 -- Use attributes formed and split for computing the lifespan
 
-SELECT band_name, IFNULL(split, 2020)-IFNULL(formed, 0) AS lifespan
+SELECT band_name, IFNULL(split, 2022)-IFNULL(formed, 0) AS lifespan
 FROM metal_bands LIKE '%Glam rock%' ORDER BY lifespan DESC;

8e80fc3986c30f238c0059090282824f425a67a2
Alexoat76 <3915@holbertonschool.com>
2022-08-17 15:25:57 -0500

    Testing Answer

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python3
-""" Implementing an expiring web cache and tracker
-    obtain the HTML content of a particular URL and returns it """
+""" Adv task webs module """
+from requests import get as r_get
+from functools import wraps
+from typing import Callable
 import redis
-import requests
-r = redis.Redis()
-count = 0
+redis_db = redis.Redis()
+# redis_db.flushdb()
 
 
+def cache(method: Callable):
+    """ Caches website data """
+    @wraps(method)
+    def inner(*args):
+        if args:
+            url = args[0]
+            redis_db.incr(f"count:{url}")
+
+            cached_data = redis_db.get(url)
+            if cached_data:
+                return str(cached_data, 'UTF-8')
+
+            html_response = method(url)
+            redis_db.set(url, html_response, 10)
+            return html_response
+
+    return inner
+
+
+@cache
 def get_page(url: str) -> str:
-    """ track how many times a particular URL was accessed in the key
-        "count:{url}"
-        and cache the result with an expiration time of 10 seconds """
-    r.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
-    return resp.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    """ Runs a GET request on a given URL """
+    if type(url) is str:
+        req = r_get(url)
+        return req.text
